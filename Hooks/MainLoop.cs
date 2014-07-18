4a961bee31fe33e5147bb86d9d8fb652569cd6ac
Baaleos <Baaleos@azmodan.net>
2014-07-18 16:16:01 +0100

    Some random stuff - think it makes sense,
    Not sure I like how I am handling the Internals etc.
    To be honest I should be treating them more like structs instead of like
    uints

---
+++ Hooks/MainLoop.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WhiteMagic;
+using System.Threading;
+using System.Runtime;
+namespace NWNDotNet.Hooks
+{
+    class MainLoop
+    {
+        //  0x0042CA10   = SrvMainLoop;
+        public MainLoop()
+        {
+            Magic.Instance.Detours.CreateAndApply(Magic.Instance.RegisterDelegate<MainLoopDelegate>(0x0042CA10), MainLoopHandler, "MainLoop");
+
+        }
+
+        /*
+         *  0x0066c050   - AppManager  - Read Pointer
+         *  +0x04    - Read Pointer
+         *  Voila - ExoAppInternal
+         *  */
+
+
+        private static readonly MainLoopDelegate MainLoopHandler = MainLoopHook;
+
+        //Dont think we really need to do any work on the 'instance' as it will be the pThis for the CExoAppExternal : automatically provided?
+        private static void MainLoopHook(uint instance)
+        {
+        
+            //Call the original
+            Magic.Instance.Detours["MainLoop"].CallOriginal(instance);
+        }
+
+        private delegate void MainLoopDelegate(uint pThis);
+
+
+    }
+}
---
+++ NWNInternals/Internals.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WhiteMagic;
+
+namespace NWNDotNet.NWNInternals
+{
+    class Internals
+    {
+        public static uint AppMananger = 0x0066c050;
+        public static uint NWN_VirtualMachine = 0x0066C048;
+        public static uint NWN_CTlkTable = 0x0066C054;
+        public static uint NWN_CNWTlkTable = 0x0066C058;
+        public static uint NWN_Rules = 0x0066c060;
+        public static uint NWN_ResMan = 0x0066C044;
+
+
+    }
+}
